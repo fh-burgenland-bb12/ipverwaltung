f3ace73af87a5392489914da219798c16d01bf89
Lukas Plattner <lukas01@gmail.com>
2014-04-19 23:33:43 +0200

    Refactor form

--- module/Ipverwaltung/src/Ipverwaltung/Model/Firma/FirmaForm.php
+++ module/Ipverwaltung/src/Ipverwaltung/Model/Firma/FirmaForm.php
@@ -9,29 +9,32 @@ class FirmaForm extends Form
 
     public function __construct()
     {
-        parent::__construct();		$firmaId = new Element\Number('firmaId');
+        parent::__construct();
+
+        $firmaId = new Element\Number('firmaId');
 		$firmaId->setLabel('FirmaId');
 		$firmaId->setAttributes(array (
-  'type' => 'number',
-  'min' => '0',
-));
+              'type' => 'hidden',
+              'min' => '0',
+            ));
 		$this->add($firmaId);
+
 		$name = new Element\Text('name');
 		$name->setLabel('Name');
 		$name->setAttributes(array (
-  'type' => 'text',
-));
-		$this->add($name);
-            $send = new Element('submit');
-            $send->setLabel('');
-            $send->setValue('Submit');
-            $send->setAttributes(array(
-                'type'  => 'submit'
+              'type' => 'text',
             ));
+		$this->add($name);
 
-            $this->add($send);
+        $send = new Element('submit');
+        $send->setLabel('');
+        $send->setValue('Submit');
+        $send->setAttributes(array(
+            'type'  => 'submit'
+        ));
+        $this->add($send);
 
-            $csrf = new Element\Csrf('csrf');
-            $this->add($csrf);
+        $csrf = new Element\Csrf('csrf');
+        $this->add($csrf);
 	}
 }
